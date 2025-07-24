4410289bac429245e94d0a22ea70c1b3fc7ee281
lucasneiva <neiva.lucas13@gmail.com>
2025-07-24 14:58:52 -0300

    fix: feed button

--- lib/features/pet_management/presentation/widgets/pet_display.dart
+++ lib/features/pet_management/presentation/widgets/pet_display.dart
@@ -27,22 +27,23 @@ class PetDisplay extends StatelessWidget {
 
           Spacer(flex: 1),
           // Botão
-          ElevatedButton(
-            onPressed: () => showDialog(
-              context: context,
-              builder: (context) => ConfirmationDialog(
-                message: petState.isDead
-                    ? "Quer mesmo reviver seu companheiro?"
-                    : "Vai dar comida pro bichinho agora?",
-                title: petState.isDead ? "Reviver" : "Alimentar",
-                onConfirmation: onFeedPet,
+            ElevatedButton(
+              onPressed: !petState.isFed ? () => showDialog(
+                context: context,
+                builder: (context) => ConfirmationDialog(
+                  message: petState.isDead
+                      ? "Quer mesmo reviver seu companheiro?"
+                      : "Vai dar comida pro bichinho agora?",
+                  title: petState.isDead ? "Reviver" : "Alimentar",
+                  onConfirmation: onFeedPet,
+                ),
+              )
+              : null,
+              child: Text(
+                petState.isDead
+                    ? "Reviver o Bichinho"
+                    : "Alimentar",
               ),
-            ),
-            child: Text(
-              petState.isDead
-                  ? "Reviver o Bichinho"
-                  : "Alimentar",
-            ),
           ),
         ],
       ),
