263c584d91ae7407cdce5aa4ac600538ae405e43
lucasneiva <neiva.lucas13@gmail.com>
2025-07-28 09:10:54 -0300

    feat: add title to habit name card

--- lib/features/pet_management/presentation/widgets/habit_name_card.dart
+++ lib/features/pet_management/presentation/widgets/habit_name_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firy_streak/core/theme/app_colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 class HabitNameCard extends StatelessWidget {
   final String habitName;
 
@@ -11,7 +12,10 @@ class HabitNameCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16), // reduzi
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16), // reduzi
+        padding: const EdgeInsets.symmetric(
+          vertical: 12,
+          horizontal: 16,
+        ), // reduzi
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(16.0),
@@ -22,22 +26,27 @@ class HabitNameCard extends StatelessWidget {
         ),
         child: FittedBox(
           fit: BoxFit.scaleDown,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            textBaseline: TextBaseline.alphabetic,
-            mainAxisSize: MainAxisSize.min,
+          child: Column(
             children: [
-              SvgPicture.asset('assets/aim.svg', height: 24),
-              const SizedBox(width: 6),
-              Text(
-                habitName,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.darkGrey,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset('assets/aim.svg', height: 24),
+                  const SizedBox(width: 6),
+                  Text(
+                    "Hábito",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.darkGrey,
+                    ),
+                  ),
+                ],
               ),
+              Center(child: Text(habitName, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400))),
             ],
           ),
         ),
